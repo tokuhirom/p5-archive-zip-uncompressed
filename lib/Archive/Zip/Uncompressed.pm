9582015f992ba6d51a6d69030e977e5536da24fe
tokuhirom <tokuhirom@gmail.com>
2010-02-25 12:42:26 +0900

    initial import

---
+++ lib/Archive/Zip/Uncompressed.pm
@@ -0,0 +1,35 @@
+package Archive::Zip::Uncompressed;
+use strict;
+use warnings;
+use 5.00800;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+Archive::Zip::Uncompressed -
+
+=head1 SYNOPSIS
+
+  use Archive::Zip::Uncompressed;
+
+=head1 DESCRIPTION
+
+Archive::Zip::Uncompressed is
+
+=head1 AUTHOR
+
+Tokuhiro Matsuno E<lt>tokuhirom AAJKLFJEF GMAIL COME<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
